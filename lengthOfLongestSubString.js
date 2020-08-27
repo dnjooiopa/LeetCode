2b2acb1f5ea40a02d499ef35451ce21d65378efb
dnjooiopa <dnjooiopa@gmail.com>
2020-08-27 09:11:43 +0700

    finished length of longest substring

--- lengthOfLongestSubString.js
+++ lengthOfLongestSubString.js
@@ -1,16 +1,14 @@
 function lengthOfLongestSubString(s) {
     let max = 0;
-    let temp = 0;
-    let startIndex = 0;
-    let lastIndex = 0;
-    for (let i = 0; i < s.length - 1; i++) {
-        temp += 1
-        if (s[i] == s[i + 1]) {
-            max = (temp > max) ? temp : max
-            startIndex = i - max + 1
-            temp = 0
-        }
+    let leftWindow = 0;
+    let index = new Array(128).fill(0)
+
+    for (let rightWindow = 0; rightWindow < s.length; rightWindow++) {
+        leftWindow = Math.max(index[s.charCodeAt(rightWindow)], leftWindow)
+        max = Math.max(max, rightWindow - leftWindow + 1)
+        index[s.charCodeAt(rightWindow)] = rightWindow + 1
     }
+
     return max;
 }
 
